9513174c2751e2fa4e24bdded0e3f7f4337efc4b
kidisakid <neobalignasay@gmail.com>
2026-02-10 09:46:25 +0800

    remove text outputs and OOP method

--- sentiment.py
+++ sentiment.py
@@ -1,22 +1,8 @@
-# Using bert for sentiment analysis
-# https://huggingface.co/transformers/v3.0.2/model_doc/
-
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-def sentiment_analysis(texts):
-    pipeline = pipeline(
-        task='sentiment-analysis',
-        # Load the model and tokenizer from Hugging Face
-        model='distilbert-base-uncased-finetuned-sst-2-english'
-    )
-
-    for text in texts:
-        result = pipeline(text)
-        # Print the result magnitude/score as negative, neutral, or positive
-        # using score thresholds: negative (<0.4), neutral (0.4-0.6), positive (>0.6)
-
-        print(f"Text: {text}\nSentiment: {result}\n")
-
-    return result
-    
+pipeline = pipeline(
+    task='sentiment-analysis',
+    # Load the model and tokenizer from Hugging Face
+    model='distilbert-base-uncased-finetuned-sst-2-english'
+)
